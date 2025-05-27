164b31d7aaace30c7d8396a7f1f77948a7ddc765
Muhammad anas arshad <82437988+muhammadanas060@users.noreply.github.com>
2025-05-27 15:52:06 +0500

    Update silvertables.sql

--- sql_scripts/silvertables.sql
+++ sql_scripts/silvertables.sql
@@ -1,68 +1,70 @@
-DROP TABLE silver.edu_level;
-CREATE TABLE silver.edu_level(
-    EducationLevelID INT PRIMARY KEY,
-    EducationLevel VARCHAR(100) NOT NULL,
-	created_at timestamp
+CREATE TABLE silver.edu_level (
+	EducationLevelID INT,
+	EducationLevel VARCHAR(1000),
+    created_at timestamp
+)  
+
+CREATE TABLE silver.employee(
+	EmployeeID VARCHAR(1000),
+	FirstName VARCHAR(30),
+	LastName VARCHAR(30),
+	Gender VARCHAR(100),
+	Age INT,
+	BusinessTravel VARCHAR(70),
+	Department VARCHAR(70),
+	"DistanceFromHome (KM)" INT,
+	State VARCHAR(5),
+	Ethnicity VARCHAR(1000),
+	Education INT,
+	EducationField VARCHAR(50),
+	JobRole VARCHAR(1000),
+	MaritalStatus VARCHAR(50),
+	Salary INT,
+	StockOptionLevel INT,
+	OverTime VARCHAR(10),
+	HireDate DATE,
+	Attrition VARCHAR(10),
+	YearsAtCompany INT,
+	YearsInMostRecentRole INT,
+	YearsSinceLastPromotion INT,
+	YearsWithCurrManager INT,
+    created_at timestamp
+) 
+
+CREATE TABLE IF NOT EXISTS silver.Performance_Rating(
+	PerformanceID VARCHAR(1000),
+	EmployeeID VARCHAR(1000),
+	ReviewDate DATE,
+	EnvironmentSatisfaction INT,
+	JobSatisfaction INT,
+	RelationshipSatisfaction INT,
+	TrainingOpportunitiesWithinYear INT,
+	TrainingOpportunitiesTaken INT,
+	WorkLifeBalance INT,
+	SelfRating INT,
+	ManagerRating INT,
+    created_at timestamp
 )
-DROP TABLE silver.rating_level;
-CREATE TABLE silver.rating_level (
-    RatingID INT PRIMARY KEY,
-    RatingLevel VARCHAR(100) NOT NULL,
-	created_at timestamp
-);
-DROP TABLE silver.satisfied_level;
-CREATE TABLE silver.satisfied_level (
-    SatisfactionID INT PRIMARY KEY,
-    SatisfactionLevel VARCHAR(100) NOT NULL,
-	created_at timestamp
-);
-DROP TABLE silver.Employee;
-CREATE TABLE silver.Employee (
-    EmployeeID VARCHAR(20) PRIMARY KEY,
-    FirstName VARCHAR(50),
-    LastName VARCHAR(50),
-    Gender VARCHAR(10),
-    Age INT,
-    BusinessTravel VARCHAR(50),
-    Department VARCHAR(50),
-    DistanceFromHome_KM INT,
-    State VARCHAR(20),
-    Ethnicity VARCHAR(50),
-    Education INT,
-    EducationField VARCHAR(50),
-    JobRole VARCHAR(50),
-    MaritalStatus VARCHAR(20),
-    Salary INT,
-    StockOptionLevel INT,
-    OverTime VARCHAR(5),
-    HireDate DATE,
-    Attrition VARCHAR(5),
-    YearsAtCompany INT,
-    YearsInMostRecentRole INT,
-    YearsSinceLastPromotion INT,
-    YearsWithCurrManager INT,
-	created_at timestamp,
-    FOREIGN KEY (Education) REFERENCES silver.edu_level(EducationLevelID)
-);
-DROP TABLE silver.performance_rating;
-CREATE TABLE silver.performance_rating (
-    PerformanceID VARCHAR(10) PRIMARY KEY,
-    EmployeeID VARCHAR(20),
-    ReviewDate DATE,
-    EnvironmentSatisfaction INT,
-    JobSatisfaction INT,
-    RelationshipSatisfaction INT,
-    TrainingOpportunitiesWithinYear INT,
-    TrainingOpportunitiesTaken INT,
-    WorkLifeBalance INT,
-    SelfRating INT,
-    ManagerRating INT,
-	created_at timestamp,
-    
-    FOREIGN KEY (EmployeeID) REFERENCES silver.Employee(EmployeeID),
-    FOREIGN KEY (EnvironmentSatisfaction) REFERENCES silver.satisfied_level(SatisfactionID),
-    FOREIGN KEY (JobSatisfaction) REFERENCES silver.satisfied_level(SatisfactionID),
-    FOREIGN KEY (RelationshipSatisfaction) REFERENCES silver.satisfied_level(SatisfactionID),
-    FOREIGN KEY (SelfRating) REFERENCES silver.rating_level(RatingID),
-    FOREIGN KEY (ManagerRating) REFERENCES silver.rating_level(RatingID)
+
+CREATE TABLE IF NOT EXISTS silver.rating_level(
+	RatingID INT,
+	RatingLevel VARCHAR(1000),
+    created_at timestamp
+
+)
+
+CREATE TABLE IF NOT EXISTS silver.satisfied_level(
+	SatisfactionID INT,
+	SatisfactionLevel VARCHAR(1000),
+    created_at timestamp
+
+)
+
+# --------------------------------- Log Table
+CREATE TABLE silver.load_logs (
+    table_name TEXT,
+    rows_loaded INT,
+    load_time TIMESTAMP,
+    status TEXT,
+    duration INTERVAL
 );
